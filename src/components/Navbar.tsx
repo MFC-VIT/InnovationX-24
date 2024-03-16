cd15a490d955cad66b07e18129759b5d5637fd8b
Harshit P G <harshitpg2004@gmail.com>
2024-03-17 05:05:45 +0530

    Feat: Added logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,7 +2,7 @@ const Navbar = () => {
   return (
     <div className="w-full h-full flex items-center justify-center">
       <img
-        src="/images/innovx.png"
+        src="/images/logo.png"
         alt=""
         className="w-[50%] h-auto md:w-auto md:h-full md:mt-6"
       />
